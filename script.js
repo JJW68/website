9b2e60a68b45273357f1567f246ac291a2a4cd82
Jing Wen <jing@Jings-MacBook-Air-2.local>
2023-07-16 10:38:49 -0400

    Separate html files

---
+++ script.js
@@ -0,0 +1,15 @@
+// home page
+function loadHomePage() {
+}
+
+// Toronto page
+function loadTorontoPage() {
+}
+
+// art page
+function loadArtPage() {
+}
+
+// resume page
+function loadResumePage() {
+}
